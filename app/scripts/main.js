c264bfbac9706aaa24cd025d9bcc69749ce8be8b
calidion <calidion@gmail.com>
2015-09-06 03:01:36 +0800

    enabled js data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,297 @@
 'use strict';
-
-
-
-
-$(document).ready(function () {
-  $('.links > p.lists > a > img').each(function () {
-    if ($(this).attr('data')) {
-      $(this).attr('src', $(this).attr('data'));
+var sites = [
+{
+  name: '搜索引擎',
+  items: [
+  {
+    url: 'http://www.google.com',
+    favicon: 'images/google.ico',
+    name: '谷歌'
+  },
+  {
+    url: 'http://www.bing.com',
+    favicon: 'http://www.bing.com/s/a/bing_p.ico',
+    name: '必应'
+  },
+  {
+    url: 'http://www.baidu.com',
+    favicon: 'http://www.baidu.com/favicon.ico',
+    name: '百度'
+  }]
+},
+{
+  name: '常用IDE',
+  items: [
+  {
+    url: 'http://vim.org',
+    favicon: 'http://www.vim.org/images/vim_shortcut.ico',
+    name: 'Vim'
+  },
+  {
+    url: 'http://www.gnu.org/software/emacs/',
+    favicon: 'http://www.gnu.org/graphics/gnu-head-mini.png',
+    name: 'Emacs'
+  },
+  {
+    url: 'https://www.jetbrains.com/webstorm/',
+    favicon: 'https://www.jetbrains.com/webstorm/favicon.ico',
+    name: 'Web Storm'
+  },
+  {
+    url: 'https://atom.io/',
+    favicon: 'https://atom.io/favicon.ico',
+    name: 'Atom'
+  },
+  {
+    url: 'https://code.visualstudio.com/',
+    favicon: 'https://code.visualstudio.com/Content/images/favicon.ico',
+    name: 'VS Code'
+  }]
+},
+{
+  name: '代码托管',
+  items: [
+  {
+    url: 'https://github.com',
+    favicon: 'http://www.github.com/favicon.ico',
+    name: 'GitHub'
+  },
+  {
+    url: 'http://www.bitbucket.org',
+    favicon: 'http://www.bitbucket.org/favicon.ico',
+    name: 'BitBucket'
+  },
+  {
+    url: 'http://gitlab.com',
+    favicon: 'https://gitlab.com/assets/favicon-5738a6efe01f3282080df5f467da72a9.ico',
+    name: 'GitLab'
+  }]
+},
+{
+  name: '常用UI框架',
+  items: [
+  {
+    url: 'http://getbootstrap.com',
+    favicon: 'http://getbootstrap.com/favicon.ico',
+    name: 'Bootstrap'
+  },
+  {
+    url: 'http://metroui.org.ua/',
+    favicon: 'http://metroui.org.ua/favicon.ico',
+    name: 'Metro UI'
+  },
+  {
+    url: 'http://material.angularjs.org',
+    favicon: 'images/angular2.ico',
+    name: 'Material Design'
+  },
+  {
+    url: 'http://purecss.io/',
+    favicon: 'http://purecss.io/favicon.ico',
+    name: 'Pure CSS'
+  },
+  {
+    url: 'http://ionicframework.com',
+    favicon: 'http://ionicframework.com/img/ionic-logo-blog.png',
+    name: 'Ionic'
+  },
+  {
+    url: 'http://jquerymobile.com/',
+    favicon: 'http://jquerymobile.com/favicon.ico',
+    name: 'Ionic'
+  },
+  {
+    url: 'http://app-framework-software.intel.com/',
+    favicon: 'images/intel.jpg',
+    name: 'APPFramework'
+  }]
+},
+{
+  name: 'HTML+CSS',
+  items: [
+  {
+    url: 'http://getbootstrap.com',
+    favicon: 'http://www.getbootstrap.com/favicon.ico',
+    name: 'Bootstrap'
+  },
+  {
+    url: 'http://css-tricks.com/',
+    favicon: 'http://www.css-tricks.com/favicon.ico',
+    name: 'css-tricks'
+  },
+  {
+    url: 'http://sass-lang.com/',
+    favicon: 'http://www.sass-lang.com/favicon.ico',
+    name: 'Sass'
+  },
+  {
+    url: 'http://lesscss.org/',
+    favicon: 'http://lesscss.org/public/ico/favicon.ico',
+    name: 'Less'
+  },
+  {
+    url: 'http://learnboost.github.io/stylus/',
+    favicon: 'http://learnboost.github.io/stylus/docs/graphics/Logos/stylus.png',
+    name: 'Less'
+  },
+  {
+    url: 'http://quirksmode.org/',
+    favicon: 'http://quirksmode.org/favicon.ico',
+    name: 'Quicks Mode'
+  }, ]
+},
+{
+  name: 'JS前端框架、模板类库等',
+  items: [
+  {
+    url: 'http://jquery.com/',
+    favicon: 'http://jquery.com/favicon.ico',
+    name: 'JQuery Mobile'
+  },
+  {
+    url: 'http://angularjs.org',
+    favicon: 'images/angular1.ico',
+    name: 'Angular V1.X'
+  },
+  {
+    url: 'http://angular.io/',
+    favicon: 'images/angular2.ico',
+    name: 'Angular V2.X'
+  },
+  {
+    url: 'http://emberjs.com/',
+    favicon: 'images/emberjs.jpg',
+    name: 'EmberJS'
+  },
+  {
+    url: 'http://facebook.github.io/react/',
+    favicon: 'http://facebook.github.io/react/img/logo.svg',
+    name: 'ReactJS'
+  },
+  {
+    url: 'http://handlebarsjs.com/',
+    favicon: 'http://handlebarsjs.com/images/favicon.png',
+    name: 'handlebars'
+  },
+  {
+    url: 'http://backbonejs.org/',
+    favicon: 'http://backbonejs.org/favicon.ico',
+    name: 'Backbone'
+  }]
+},
+{
+  name: 'JS后端或全栈框架',
+  items: [
+  {
+    url: 'http://expressjs.com',
+    favicon: 'http://expressjs.com/images/strongloop-logo.png',
+    name: 'Express JS'
+  },
+  {
+    url: 'http://sailsjs.org',
+    favicon: 'http://sailsjs.org/favicon.ico',
+    name: 'Sails JS'
+  },
+  {
+    url: 'http://www.meteor.com',
+    favicon: 'https://www.meteor.com/favicon.png',
+    name: 'Meteor'
+  }]
+},
+{
+  name: 'nodejs/iojs',
+  items: [
+  {
+    url: 'http://nodejs.org',
+    favicon: 'http://nodejs.org/favicon.ico',
+    name: 'Node.js'
+  },
+  {
+    url: 'https://iojs.org/',
+    favicon: 'https://iojs.org/images/1.0.0.png',
+    name: 'io.js'
+  }]
+},
+{
+  name: 'ES6+',
+  items: [
+  {
+    url: 'https://babeljs.io/',
+    favicon: 'https://babeljs.io/favicon.ico',
+    name: 'babeljs'
+  },
+  {
+    url: 'https://github.com/google/traceur-compiler',
+    favicon: 'images/google.ico',
+    name: 'traceur'
+  }]
+},
+{
+  name: '在线开发/设计网站',
+  items: [
+  {
+    url: 'http://jsfiddle.net/',
+    favicon: 'http://jsfiddle.net/favicon.ico',
+    name: 'JS Fiddle'
+  },
+  {
+    url: 'https://codepen.io/',
+    favicon: 'https://codepen.io/favicon.ico',
+    name: 'Code Pen'
+  }]
+},
+{
+  name: 'JS引擎',
+  items: [
+  {
+    url: 'http://asm.js/',
+    favicon: 'favicon.ico',
+    name: 'ASM.JS'
+  },
+  {
+    url: 'https://code.google.com/p/v8/',
+    favicon: 'images/v8.png',
+    name: 'V8'
+  }]
+},
+{
+  name: '短信平台',
+  items: [
+  {
+    url: 'https://www.twilio.com/',
+    favicon: 'https://www.twilio.com/marketing/bundles/marketing/img/favicons/favicon.ico',
+    name: 'twilio'
+  },
+  {
+    url: 'http://www.yuntongxun.com/',
+    favicon: 'http://www.yuntongxun.com/favicon.ico',
+    name: '云通讯'
+  }]
+},{
+    name: '云计算',
+  items: [
+    {url: 'http://aws.amazon.com/',
+      favicon: 'https://a0.awsstatic.com/main/images/site/favicon.ico',
+      name: 'Amazon'
+    },
+    {
+      url: 'http://azure.microsoft.com/',
+      favicon: 'http://acom.azurecomcdn.net/80C57D/cdn/images/cvt-90cdaf487716184e4034000935c605d1633926d348116d198f355a98b8c6cd21/icon/favicon.ico?t=popn',
+      name: 'Azure'
     }
-  });
-  $('.links > p.lists > a').click(function () {
-    if ($(this).attr('data')) {
-      $(this).attr('src', $(this).attr('data'));
-    }
-  });
+  ]
+
+
+  }
+];
+$(document).ready(function ()
+{
+  var source = $('#section').html();
+  var template = window.Handlebars.compile(source);
+  for(var i = 0; i < sites.length; i++)
+  {
+    var item = sites[i];
+    console.log(template(item));
+    $('.links').append(template(item));
+  }
 });
