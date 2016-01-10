c94d1935f58c63964b0ce2de881de0075613d745
calidion <calidion@gmail.com>
2016-01-11 06:07:32 +0800

    added more

--- app/scripts/sites.js
+++ app/scripts/sites.js
@@ -391,4 +391,16 @@ window.sites = [{
     name: 'GNU',
     title: '自由软件GNU项目'
   }]
+}, {
+  name: '静态博客工具或服务',
+  items: [{
+    url: 'http://surge.sh/',
+    favicon:'http://surge.sh/images/logos/svg/surge-logo.svg',
+    name: 'surge.sh',
+    title: 'surge.sh静态网站服务'
+  }, {
+    url: 'https://hexo.io/',
+    favicon: 'https://hexo.io/icon/apple-touch-icon-152x152.png',
+    name: 'hexo'
+  }]
 }];
