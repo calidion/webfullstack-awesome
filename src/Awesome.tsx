8c087c77b84f6ae045d0db442cef3d85b144598e
calidion <calidion@gmail.com>
2022-07-12 16:39:56 +0800

    enabled sites

--- src/Awesome.tsx
+++ src/Awesome.tsx
@@ -1,21 +1,44 @@
 import { useEffect, useState } from "react";
 import "./scss/awesome.scss";
+import Sites from "./Sites";
 
 function Awesome() {
   let [percentage, setPercentage] = useState(0);
-  // let [isStarted, setStarted] = useState(false);
+  let [isFetched, setIsFetched] = useState(false);
+  let [sites, setSites] = useState(null);
+  let [error, setError] = useState(null);
 
   console.log("awesome", percentage);
+
+  const fetchSites = () => {
+    fetch("./data/sites.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log("sites", json);
+        setSites(json);
+        setIsFetched(true);
+      })
+      .catch((e) => {
+        setError(error);
+      });
+  };
 
   useEffect(() => {
     console.log("useEffect");
     let timer = setInterval(() => {
-      if (percentage>= 100) {
+      if (percentage >= 100 || isFetched) {
         setPercentage(100);
+        clearInterval(timer);
       } else {
         setPercentage(percentage + 1);
       }
     }, 50);
+
+    if (!isFetched) {
+      fetchSites();
+    }
 
     return () => clearInterval(timer);
   });
@@ -23,19 +46,26 @@ function Awesome() {
   return (
     <div className="row">
       <div className="col-md-12 links">
-        <div className="progress">
-          <div
-            className="progress-bar progress-bar-success progress-bar-striped active"
-            role="progressbar"
-            aria-valuenow="100"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{ width: `${percentage}%` }}
-          ></div>
-        </div>
-        <p className="alert alert-info text-center">
-          正在加载中...如果长时间没有关闭此进度条，可能是你的浏览器不支持当前的网页
-        </p>
+        {!isFetched && (
+          <div className="progress">
+            <div
+              className="progress-bar progress-bar-success progress-bar-striped active"
+              role="progressbar"
+              aria-valuenow="100"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style={{ width: `${percentage}%` }}
+            ></div>
+          </div>
+        )}
+        {!isFetched && (
+          <p className="alert alert-info text-center">
+            正在加载中...如果长时间没有关闭此进度条，可能是你的浏览器不支持当前的网页
+          </p>
+        )}
+        {
+          isFetched && <ul className="root" ><Sites item={sites}/> </ul>
+        }
       </div>
     </div>
   );
---
+++ src/Sites.tsx
@@ -0,0 +1,26 @@
+import "./scss/awesome.scss";
+
+function Sites(props: any) {
+  return (
+    <li className="title">
+      {props.item.url && (
+        <a
+          target="_blank"
+          title={props.item.name}
+          href={props.item.url}
+          className="item"
+        >
+          <img src={props.item.favicon || "images/loading.gif"} />
+        </a>
+      )}
+      {!props.item.url && <h3>{props.item.name}</h3>}
+      {
+        props.item.items && props.item.items.map((item) => {
+          return <Sites item={item}/>
+        })
+      }
+    </li>
+  );
+}
+
+export default Sites;
